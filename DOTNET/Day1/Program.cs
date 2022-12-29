c8c33e2bc61baca0aa122c256cda6b76572fd5f2
Abhishek Singh <115094053+work-abhisheksingh@users.noreply.github.com>
2022-12-29 17:28:21 +0530

    Create Program.cs

---
+++ DOTNET/Day1/Program.cs
@@ -0,0 +1,7 @@
+using ComplexNumber;
+Console.WriteLine("ABC");
+Console.WriteLine("Hello this is my first programm");
+Complex  c1=new Complex(2,5);
+Complex c2=new Complex(1,8);
+Complex c3=c1 + c2;
+Console.WriteLine(c3.Real+ " " +c3.Imaginary);
