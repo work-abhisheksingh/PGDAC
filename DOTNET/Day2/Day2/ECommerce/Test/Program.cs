a6310e1d2e60dc44101fb6fa7a6dcf501ff7e2be
Abhishek Singh <115094053+work-abhisheksingh@users.noreply.github.com>
2023-01-01 09:14:42 +0530

    E commerce Project

---
+++ DOTNET/Day2/Day2/ECommerce/Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using Membership;
+using HR;
+
+
+//Entry Point code for ECommerce Solution
+
+Console.WriteLine( "TFL Store ");
+Console.WriteLine("******************************");
+
+Console.WriteLine("Enter your Email ID");
+string email=Console.ReadLine();
+
+Console.WriteLine("Enter your password");
+string password=Console.ReadLine();
+
+if(AuthManager.Validate(email,password))
+{
+    Console.WriteLine("Welcome to Transflower Store");
+    Employee emp=new SalesManager();
+    emp.DoWork();
+    Console.WriteLine(emp);
+    float salary=emp.ComputePay();
+    Console.WriteLine("Salary = "+ salary);
+}
+else{
+     Console.WriteLine("Invalid User");
+  
+}
+
+Console.WriteLine("Thank you for visiting Transflower");
